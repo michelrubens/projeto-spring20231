dc5924ddddad1bb08df8aaf9a8a9191738d92bd9
michelrubens <michelrube@gmail.com>
2023-03-07 20:27:46 -0300

    Initial commit

---
+++ src/main/java/br/gov/sp/fatec/projetospring20231/ProjetoSpring20231Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetospring20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring20231Application.class, args);
+	}
+
+}
